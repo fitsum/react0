3d38ec7500caa2efb5a947cd308079ba0f00af02
fitsum <fitsumbelay@gmail.com>
2024-08-20 23:13:01 -0400

    update with ornot project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+/* import ReactDOM from 'react-dom/client'; */
+/* import logo from './logo.svg'; */
+/* import './App.css'; */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MyList = () => {
+  const weather = useRef( null )
+  useEffect( () => {
+    ( async () => {
+      weather.current = ( await ( await fetch( 'https://dumbservices.com:9994/forecasts/current' ) ).json() ).result[ 0 ]
+
+    } )()
+  }, [] )
+  const listKeyStyle = { fontVariant: 'none' }
+  const listValueStyle = { fontStyle: 'italic' }
+  const listStyle = { overflow: 'scroll', listStyleType: 'none', paddingInlineStart: '0.5rem' }
+  const currentWeather = weather.current ?? {}
+  const forecast = [ ...Object.entries( currentWeather ) ]
+    .filter( entry => entry[ 1 ] )
+    .map( item => <li><span style={listKeyStyle}>{item[ 0 ]}</span> <span style={listValueStyle}>{item[ 1 ]}</span></li> )
+  console.log(weather)
+  return ( <ul style={listStyle}>{forecast}</ul> )
 }
+
+
+const App = () => {
+  const style = { overflow: 'scroll' }
+  return (
+    <div style={style}>
+      <h1>Welcome to my app</h1>
+      <MyList />
+    </div>
+  )
+}
+
 
 export default App;
