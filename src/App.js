6f95658ee53ce7025cc33a3601a344aae20fcc5d
fitsum <fitsumbelay@gmail.com>
2024-11-02 18:43:15 -0400

    landmark commit: get useEffect to render something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useState} from 'react';
 
 const MyList = () => {
-  const weather = useRef( null )
+  const [ weather, setWeather ] = useState( null )
   useEffect( () => {
     ( async () => {
-      weather.current = ( await ( await fetch( 'https://dumbservices.com:9994/forecasts/current' ) ).json() )
+      /* fetch( 'https://dumbservices.com:9994/forecasts/current' ).then(data => data.json()).then(weather => setWeather(weather) ) */
+      setWeather('<li>1</li><li>2</li>')
     } )()
-  }, [] )
+  }, [ weather ] )
 
   const styles = {
     forKey: {fontVariant: 'none'},
     ForValue: {fontStyle: 'italic'},
     forList: {overflow: 'scroll', listStyleType: 'none', paddingInlineStart: '0.5rem'}
   }
-  const currentWeather = weather.current ?? {}
-  const forecast = [ ...Object.entries( currentWeather ) ]
-    .filter(entry => !['id'].includes(entry[0]))
-    .filter( entry => entry[ 1 ] )
-    .map( item => <li key={item[0]}><span style={styles.forKey}>{item[ 0 ]}</span> <span style={styles.forValue}>{item[ 1 ]}</span></li> )
-  return ( <ul style={styles.forList}>{forecast}</ul> )
+  /* const forecast = [ ...Object.entries( weather ) ] */
+  /*   .filter(entry => !['id'].includes(entry[0])) */
+  /*   .filter( entry => entry[ 1 ] ) */
+  /*   .map( item => <li key={item[0]}><span style={styles.forKey}>{item[ 0 ]}</span> <span style={styles.forValue}>{item[ 1 ]}</span></li> ) */
+  console.log({weather})
+  return ( <ul style={styles.forList}>{weather}</ul> )
 }
 
 
